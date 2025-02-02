8cc1b2fb869aad7ef6ceed88558a7e2790e348f9
kaumans <jokerkaua123@gmail.com>
2025-02-02 01:21:04 -0300

    interactions, logic, styles

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const fetchProducts = async () => {
     return products;
 };
 
-
 const generateProductHtmlContent = (product) => {
     return `
         <div class="flex gap-2 px-2 h-full hover:scale-102 hover:bg-amber-50 duration-150 p-2 rounded-xl">
@@ -28,7 +27,7 @@ const generateProductHtmlContent = (product) => {
                 </div>
                 <div class="flex justify-between px-2 w-full">
                     <p class="font-medium">R$ ${product.price.toFixed(2)}</p>
-                    <button class="bg-gray-700 w-12 cursor-pointer rounded hover:scale-105 duration-150 transition-colors ease-in-out active:bg-gray-950 hover:duration-300"
+                    <button class="add-product bg-gray-700 w-12 cursor-pointer rounded hover:scale-105 duration-150 transition-colors ease-in-out active:bg-gray-950 hover:duration-300"
                         data-name="${product.name}" data-price="${product.price}">
                         <i class="fa-solid fa-cart-plus text-white"></i>
                     </button>
@@ -47,5 +46,57 @@ const insertProductsInHtml = async () => {
     });
 };
 
-// Main
+//Vars
+let order = []
+
+//Elements
+const main = document.getElementById("main");
+const orderbtn = document.getElementById("orderbtn");
+const modalcart = document.getElementById("modalcart");
+const closemodal = document.getElementById("closemodal");
+const ordersquant = document.getElementById("ordersquant");
+
+
+//Events
 document.addEventListener('DOMContentLoaded', insertProductsInHtml);
+
+orderbtn.addEventListener("click", () => {
+    modalcart.style.display = "flex";
+});
+
+closemodal.addEventListener("click", () => {
+    modalcart.style.display = "none";
+});
+
+modalcart.addEventListener("click", (event) => {
+    if(event.target === modalcart) { 
+        modalcart.style.display = "none";
+    }
+});
+
+main.addEventListener("click", (event) => {
+
+    //Pega o botão, se for clicado no botão ou se for em um elemento filho do botão. Busca por query
+    let addButton = event.target.closest(".add-product");
+
+    if(addButton) {
+        // const product =  {name: addButton.getAttribute("data-name"), price: parseFloat(addButton.getAttribute("data-price"))}
+        const product = {...addButton.dataset, price: parseFloat(addButton.dataset.price)}
+
+        // console.log(product);
+        addProductToOrder(product)
+
+        ordersquant.innerText = parseInt(ordersquant.innerText)+1
+    }
+
+});
+
+const addProductToOrder = (product) => { 
+    const sameProduct = order.find(products => products.name === product.name)
+    // console.log(sameProduct);
+    if(sameProduct){
+        sameProduct.quant += 1; // pointers?
+    } else {
+        order.push({...product, quant: 1});
+    }
+};
